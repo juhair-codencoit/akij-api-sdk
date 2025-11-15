d93175aeb7c91f692759c8bda6760251c380eb31
juhair-codencoit <juhair.fahmid@codencoit.com>
2025-11-15 21:02:01 +0600

    Trying Search Function

--- client.py
+++ client.py
@@ -17,6 +17,40 @@ class FlightSearchClient:
             "secretecode": self.secret_code
         }
     
+    def create_search_request(self,
+                              dcity,
+                              acity,
+                              ddate,
+                              adult,
+                              child,
+                              infant,
+                              child_age = [],
+                              infant_age = [],
+                              preferred_carrier=[],
+                              non_stop_flight="any",
+                              baggage_option="any",
+                              booking_class="Economy",
+                              language="en",
+                              supplier_uid="F1EC00009",
+                              short_ref=""
+                              ):
+        journey_type = "OneWay"
+        dcity = dcity if dcity else None
+        acity = acity if acity else None
+        ddate = ddate if ddate else None
+        adult = adult if adult else None
+        child = child if child else None
+        infant = infant if infant else None
+        child_age = child_age if child_age else []
+        infant_age = infant_age if infant_age else []
+        preferred_carrier = preferred_carrier if preferred_carrier else []
+        non_stop_flight = "any"
+        baggage_option = "any"
+        booking_class = "Economy"
+        language = "en"
+        supplier_uid = "F1EC00009"
+        short_ref = ""
+
     def search_flights(self, search_params):
         response = requests.post(f"{self.base_url}/flight/search", 
                                  json=search_params, 
